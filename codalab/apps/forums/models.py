2aec8840a3e8b3975a6b9073cb650bf0034bc8b5
ckravit <ckravit@g.ucla.edu>
2023-07-25 00:12:11 +0000

    updating codalab/apps/forums/models.py - urlresolvers module fix

--- codalab/apps/forums/models.py
+++ codalab/apps/forums/models.py
@@ -1,6 +1,8 @@
 import datetime
 
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
+
 from django.db import models
 
 from .helpers import send_mail
